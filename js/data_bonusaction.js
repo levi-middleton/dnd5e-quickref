0f7bc6bb7cd64b85699d2fd73fe9518f8059f433
Levi Middleton <levi.middleton@ibm.com>
2019-09-11 12:00:45 -0400

    Added Overrun and Tumble bonus actions

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -32,5 +32,30 @@ data_bonusaction = [
         bullets: [
 
         ]
+    },
+	{
+        title: "Overrun",
+        icon: "glowing-hands",
+        subtitle: "Rush past an enemy",
+        description: "Force your way through an enemy's space",
+        reference: "DMG, pg. 272.",
+        bullets: [
+			"When a creature tries to move through a hostile creature's space, the mover can try to force its way through by overrunning the hostile creature.",
+			"As an action or bonus action, the mover makes a Strength (Athletics) check contested by the hostile creature's Strength (Athletics) check.",
+			"The creature attempting the overrun has advantage on the check if it is larger than the hostile creature, or disadvantage if it is smaller.",
+			"If the mover wins the contest, it can move through the hostile creature's space once this turn."
+        ]
+    },
+	{
+        title: "Tumble",
+        icon: "dodging",
+        subtitle: "Dodge past an enemy",
+        description: "Dodge your way through an enemy's space",
+        reference: "DMG, pg. 272.",
+        bullets: [
+			"When a creature tries to move through a hostile creature's space, the mover can try to duck and weave past the hostile creature.",
+			"As an action or bonus action, the mover makes a Dexterity (Acrobatics) check contested by the hostile creature's Dexterity (Acrobatics) check.",
+			"If the mover wins the contest, it can move through the hostile creature's space once this turn."
+        ]
     }
 ]
